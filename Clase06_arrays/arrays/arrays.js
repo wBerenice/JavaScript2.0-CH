cea8a2f2cf2fe5f5e5821fec242766423b7ee377
Berenice Sanchez <sanchezberenice09@gmail.com>
2022-08-02 12:59:06 -0500

    Commit arrays

---
+++ Clase06_arrays/arrays/arrays.js
@@ -0,0 +1,5 @@
+const nombres = ['Rita', 'Pedro', 'Miguel', 'Ana', 'Vanesa'];
+
+console.log( nombres.indexOf('Rita') ) // ⇒ 0
+console.log( nombres.indexOf('Ana') ) // ⇒ 3
+console.log( nombres.indexOf('Julieta') ) // ⇒ -1
