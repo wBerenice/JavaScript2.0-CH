0b27f9396ddcb57c5c383a8d28b069ac68747f4a
Berenice Sanchez <sanchezberenice09@gmail.com>
2022-08-03 00:01:52 -0500

    commit de desafio y funciones

--- Clase04_funciones/funciones.js
+++ Clase04_funciones/funciones.js
@@ -1,66 +1,125 @@
-//Funciones sin parametros
+// Funciones sin parametros
 // function saludar () {
 //     let nombre = prompt("Cual es tu nombre?")
 //     console.log("Buenos dias " + nombre);
 // }
+// saludar();
 
-// saludar ();
+
+//-------------------------------------------------------------------------
 
 //funciones con parametros
 
 // function sumarNumeros(num1, num2){
-//     let suma = num1 + num2;
-//     console.log("La suma es " + suma);
-// }
+//         let suma = num1 + num2;
+//         console.log("La suma de " + num1 + " + " + num2 + " es " + suma); 
 
-// sumarNumeros(2,8);
+//     }
+//     sumarNumeros(3,11);
 
-// let primerNum = parseInt(prompt("Ingresa el primer numero a sumar")); //2
+//     let primerNum = parseInt(prompt("Ingresa el primer numero a sumar")); //2
+    
+//     let segundoNum = parseInt(prompt("Ingresa el segundo numero a sumar")); //1
+    
+//     sumarNumeros(primerNum, segundoNum);
 
-// let segundoNum = parseInt(prompt("Ingresa el segundo numero a sumar")); //1
+//     //manera corta
+//     sumarNumeros(parseInt(prompt("Ingresa el primer nuemro a sumar")),parseInt(prompt("Ingresa el segundo numero a sumar")));
 
-// sumarNumeros(primerNum, segundoNum)
-
-
-// function sumarNumeros(num1, num2){
-//     let suma = num1 + num2;
-//     console.log("La suma de " + "+ num1 +" + );
-// }
-
-// sumarNumeros(2,8);
-
-// let primerNum = parseInt(prompt("Ingresa el primer numero a sumar")); //2
-
-// let segundoNum = parseInt(prompt("Ingresa el segundo numero a sumar")); //1
-
-// sumarNumeros(primerNum, segundoNum)
-
-
-
+    
+//-------------------------------------------------------------------------
+    
 //Funciones con parametros y return
+
+
 // function calcularIva (precio){
-//     let precioConIva = precio * 1.21;
-//     // console.log("El precio con iva es $" + precioConIva);
-//     return precioConIva;
+        // let precioConIva = precio * 1.21;
+        // console.log("El precio con iva es $" + precioConIva);
+        // return precioConIva; 
 // }
 
-// let conIva = calcularIva(125); //retorna 121
-// console.log("El precio con el IVA es $" + conIva)
+    //calcularIva(100); //RETORNA 121 (este valor 121, tiene que ser recibido por algo). Siempre que retorno algo, ese resultado tiene que ser recibido por una variable en el exterior.
+
+    // let conIva = calcularIva(100); //La variable (conIva) se va a quedar con el valor retornado (121)
+    // console.log("El precio con el IVA es $"+conIva);
+
+
 
 // function aplicarDescuento(precio){
-//     //aplico 10% de descuento al precio
-//     return precio * 0.9
+    //aplico 10% de descuento al precio
+    // return precio * 0.9
 // }
 
 // let precioConDesc = aplicarDescuento(1000);
-// console.log("El precio con el IVA es $" + precioConDesc);
+// console.log("El precio con el descuento es $" + precioConDesc);
+
+// let precioConDesc = aplicarDescuento(calcularIva(parseInt(prompt("Ingresa el precio y te dire el valor final con iva y descuento"))));
+// console.log(precioConDesc);
+
+// -------------------------------------------------------------------------
+
+//FUNCIONES ANONIMAS
+
+// const suma = function (a,b) {return a + b}
+// console.log(suma(5,10));
+
+// -------------------------------------------------------------------------
 
 
-const suma  = function (a, b) { return a + b }
-const resta = function (a, b) { return a - b }
-console.log( suma(15,20) )
-console.log( resta(15,5) )
+//FUNCIONES FLECHA =>
+
+// const suma = (a,b) => {return a + b}
+
+// //Si es una funcion de una sola linea CON RETORNO podemos evitar escribir el cuerpo {}
+// const resta = (a,b) => a - b;
+// console.log(suma(15,20))
+// console.log(resta(20,5))
+
+
+
+// -------------------------------------------------------------------------
+//Ejemplo: calcular precio
+
+
+
+// const sumaExtra = (a,b) => a + b
+// const restaExtra = (a,b) => a - b
+
+// //Si una funcion es una sola linea con retorno y tiene solo UN PARAMETRO, entonces puedes evitar escribir los ()
+
+// const iva = x => x * 0.21
+// let precioProducto = 500;
+// let descuento = 50;
+
+// //Calculo el precioProducto + IVA - descuento
+// let nuevoPrecio = restaExtra(sumaExtra(precioProducto, iva (precioProducto)), descuento)
+// console.log(nuevoPrecio);
 
 
 
 
+// -------------------------------------------------------------------------
+
+//Ejemplo en clase= Funcion flecha
+
+// const direccion = (calle, numero) => calle + " " + numero;
+
+// console.log(direccion("Uruguay", "1256"));
+// console.log(direccion("Av. Rivadavia", "501"));
+
+// alert(direccion("9 de Julio", "2000"));
+// let direccionCompleta = direccion ("Av. Maipu", "1090");
+
+// const areaReact = (base, altura) => (base*altura)/2;
+
+// console.log("El area es: " + areaReact(10,5));
+
+
+// const saludo = () => {
+//     let nombre = prompt("Ingresa tu nombre")
+//     let apellido = prompt("Ingresa tu apellido")
+//     console.log("Tu nombre completo es "+ nombre + " " + apellido)
+
+// }
+
+// saludo();
---
+++ Clase04_funciones/simulador_interactivo/simulador.js
@@ -0,0 +1,36 @@
+
+
+alert("Bienvenido a Tienda Sunny. Debido al limitado stock, sólo puedes elegir hasta 5 productos de nuestra tienda.")
+
+function sumaProductos(pr1, pr2, pr3, pr4, pr5){
+        let suma = pr1 + pr2 + pr3 + pr4 + pr5;
+        console.log("El precio total de tus productos sería de: $" + suma); 
+        if (suma >= 300){
+            alert("Gracias por haber comprado más de $300. La tienda te regala un 20% de descuento en tu compra!")
+            let precioDescuento = suma * 0.8;
+            console.log("$" + suma + " era el precio que ibas a pagar, pero ahora con tu 20% de descuento vas a pagar sólo $" + precioDescuento)
+        }
+    }
+    
+
+    
+    let producto1 = parseInt(prompt("¿Cuál sería el precio de tu 1er producto? \n Gafas:  $40 \n Sombrero:  $100 \n Flotador:  $150 \n Bloqueador:  $50 \n Googles:  $20"));
+    
+    let producto2 = parseInt(prompt("¿Cuál sería el precio de tu 2do producto? \n Gafas:  $40 \n Sombrero:  $100 \n Flotador:  $150 \n Bloqueador:  $50 \n Googles:  $20"));
+
+    let producto3 = parseInt(prompt("¿Cuál sería el precio de tu 3er producto? \n Gafas:  $40 \n Sombrero:  $100 \n Flotador:  $150 \n Bloqueador:  $50 \n Googles:  $20")); 
+
+    let producto4 = parseInt(prompt("¿Cuál sería el precio de tu 4to producto? \n Gafas:  $40 \n Sombrero:  $100 \n Flotador:  $150 \n Bloqueador:  $50 \n Googles:  $20")); 
+    
+    let producto5 = parseInt(prompt("¿Cuál sería el precio de tu 5to producto? \n Gafas:  $40 \n Sombrero:  $100 \n Flotador:  $150 \n Bloqueador:  $50 \n Googles:  $20")); 
+    
+    sumaProductos(producto1, producto2, producto3, producto4, producto5);
+
+    alert("Gracias por comprar en Tienda Sunny!")
+
+
+ 
+
+
+
+
