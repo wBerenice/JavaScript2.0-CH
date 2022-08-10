4961d24052da856ff8df6eebbc867fcbe09845e8
Berenice Sanchez <sanchezberenice09@gmail.com>
2022-08-09 21:02:27 -0500

    commit 08 agosto

---
+++ Clase08_DOM/dom.js
@@ -0,0 +1,24 @@
+//DOM
+
+console.dir(document.body);
+
+//getElementById
+let seccion = document.getElementById("principal");
+
+// console.log(seccion.innerHTML); 
+//visualizo lo que tiene esa seccion
+
+//Cambiando el color de fondo a amarillo
+seccion.style.background = "#F75E25";
+
+
+let titulo = document.getElementById("titulo");
+titulo.innerText = "Hoy cambia todo!"
+
+titulo.style.font = "bold 40px monospace"
+
+
+let paises = document.getElementsByClassName("paises");
+console.log(paises[0].innerHTML);
+console.log(paises[1].innerHTML);
+console.log(paises[2].innerHTML);
